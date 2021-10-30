812520afaaea48bfd3e62c286825d906a93c68d2
pratyanshupandey <pratyan2001@gmail.com>
2021-10-30 22:05:58 +0530

    Added ABC Panasonic 2020

---
+++ Contests/Atcoder/Panasonic2020/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pr pair<ll, ll>
+#define tp tuple<ll, ll, ll>
+using namespace std;
+long long int mod = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll arr[]  ={1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    ll k;
+    cin >> k;
+    cout << arr[k-1] << endl;
+    return 0;
+}
