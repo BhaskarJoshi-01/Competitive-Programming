c31dab03b88b73fa6ba327d4805f9a99d5207529
pratyanshupandey <pratyan2001@gmail.com>
2021-10-30 19:34:58 +0530

    Adding contest solutions for CF R633 div2

---
+++ Contests/Codeforces/R633 div2/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pr pair<ll, ll>
+#define tp tuple<ll, ll, ll>
+using namespace std;
+long long int mod = 1e9 + 7;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n << endl;
+    }
+    return 0;
+}
